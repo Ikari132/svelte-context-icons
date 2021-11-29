9534cf6acc4ab950eecf69be7226120a64a4373c
Anton Aksionau <ikari808@gmail.com>
2021-11-29 22:55:50 +0100

    update d.ts, change index.ts to js, version 0.1.4

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,47 +1,18 @@
 import { SvelteComponentTyped } from "svelte";
+export type TIconProps<T = string> = {
+	name: T;
+	size?: number;
+	viewBox?: string;
+};
+export type TProviderProps = {
+	paths: Record<string, string>;
+};
 
-declare module '@ayalab/svelte-context-icons' {
-	import Icon from "./components/Icon.svelte";
-	import IconsProvider from "./components/Provider.svelte";
-	export const iconsKey: unique symbol;
-	export { Icon, IconsProvider };
-	export type TIconProps<T = string> = {
-	    name: T;
-	    size?: number;
-	    viewBox?: string;
-	};
-	export type TProviderProps = {
-	    paths: Record<string, string>;
-	};
-}
-
-declare module '@ayalab/svelte-context-icons/components/Icon.svelte' {
-	interface IconProps {
-		name: string;
-		size: any;
-		viewBox: any;
-	}
-
-	class Icon extends SvelteComponentTyped<
-		IconProps,
-		{  },
-		{  }
+declare module "@ayalab/svelte-context-icons" {
+	export class Icon extends SvelteComponentTyped<TIconProps, {}, {}> {}
+	export class IconsProvider extends SvelteComponentTyped<
+		{ paths: Record<string, string> },
+		{},
+		{}
 	> {}
-
-	export default Icon;
 }
-
-declare module '@ayalab/svelte-context-icons/components/Provider.svelte' {
-	interface ProviderProps {
-		paths: any;
-	}
-
-	class Provider extends SvelteComponentTyped<
-		ProviderProps,
-		{  },
-		{  }
-	> {}
-
-	export default Provider;
-}
-
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as Icon } from "./components/Icon.svelte";
+export { default as IconsProvider } from "./components/Provider.svelte";
+export const iconsKey = Symbol("context-icons");
