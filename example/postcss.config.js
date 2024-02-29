3ca3bba0fba7d0cc9927bb94581c098e1296eab9
Anton Aksionau <ikari808@gmail.com>
2024-02-29 22:28:29 +0100

    remove example folder

--- example/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require("postcss-utilities"),
-    require("postcss-preset-env")({ stage: 0 }),
-    require("precss"),
-    require("cssnano")({ preset: "advanced" }),
-  ],
-};
